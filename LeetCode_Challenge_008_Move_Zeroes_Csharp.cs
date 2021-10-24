a2e804af2cdda8a9b74b0ac0a8a9aa8ae227be41
Catherine <92683405+catherinecv@users.noreply.github.com>
2021-10-24 09:35:02 -0400

    Initial commit

---
+++ LeetCode_Challenge_008_Move_Zeroes_Csharp.cs
@@ -0,0 +1,24 @@
+        ## MoveZeroes (int[] nums)   
+        {
+            int count = 0;
+            int i = 0;
+            while(i < nums.Length-1-count)
+            {
+                if (nums[i] != 0)
+                {
+                    i++;
+                }
+                else //nums[i] == 0
+                {
+                    for(int j = i; j < nums.Length-1-count; j++)
+                    {
+                        int temporary;
+                        temporary = nums[j+1];
+                        nums[j+1] = nums[j];
+                        nums[j] = temporary;
+                    }
+                count++;
+                }
+            }
+            return nums;
+        }
